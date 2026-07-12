d924e8690f416696974f654daba1e9ba2f36b35a
Willard Råborg <willardraborg@gmail.com>
2026-07-12 20:17:01 +0200

    added command routing

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("momentum: Hello")
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+
+	switch os.Args[1] {
+	case "log":
+		fmt.Println("log: not implemented yet")
+	case "streak":
+		fmt.Println("streak: not implemented yet")
+	case "stats":
+		fmt.Println("stats: not implemented yet")
+	case "list":
+		fmt.Println("list: not implemented yet")
+	case "graph":
+		fmt.Println("graph: not implemented yet")
+	default:
+		fmt.Printf("momentum: unknown command %q\n", os.Args[1])
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Println("usage: momentum <command>")
+	fmt.Println("\n")
+	fmt.Println(" log: \"<note>\" <hours>   log today as shipped")
+	fmt.Println(" streak: show current streak")
+	fmt.Println(" stats: show total days and hours")
+	fmt.Println(" list <number>: show latest <number> of logged days")
+	fmt.Println(" graph: show graph of days shipped")
 }
